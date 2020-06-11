7737b66b8102b4f4e7b6bb05c149ff2d6855fa8c
peterslight <uremgbachisom@gmail.com>
2020-06-11 23:51:20 +0100

    Primary setup
    add required project dependencies
    add required project fonts
    add project drawables
    add primary project colors
    create project package hierarchy

--- app/src/main/java/com/peterstev/peterstevuremgba/activities/MainActivity.kt
+++ app/src/main/java/com/peterstev/peterstevuremgba/activities/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.peterstev.peterstevuremgba
+package com.peterstev.peterstevuremgba.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.peterstev.peterstevuremgba.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
